990f698c293a14d0d47d0afaa8798f541d58c9c2
OOOOOseulgi <tmfrl0634200@naver.com>
2023-02-11 23:57:39 +0900

    3주차 앱개발 내용

--- pages/AboutPage.js
+++ pages/AboutPage.js
@@ -1,82 +1,88 @@
-import React from 'react'
-import {StyleSheet, View, Text, ScrollView, Image, TouchableOpacity} from 'react-native'
+import React,{useEffect} from 'react'
+import {View,Text,StyleSheet,Image, TouchableOpacity} from 'react-native'
+import { StatusBar } from 'expo-status-bar';
 
-const main = 'https://storage.googleapis.com/sparta-image.appspot.com/lecture/about.png';
-export default function AboutPage(){
-  return (
-    <ScrollView style={styles.container}>
-        <Text style={styles.title}>HI! 스파르타코딩 앱개발 반에 오신것을 환영합니다</Text>
-
-        <View style={styles.imageView}>
-            <Image style={styles.mainImg} source={{uri:main}}></Image>
-            <Text style={styles.subTitle1}>많은 내용을 간결하게 담아내려 노력했습니다!</Text>
-            <Text style={styles.subTitle2}>꼭 완주 하셔서 꼭 여러분것으로 만들어가시길 바랍니다!</Text>
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>seulgi__0806</Text>
-            </TouchableOpacity>
-        </View>
-
-    </ScrollView>)
+export default function AboutPage({navigation,route}){
+    const aboutImage = "https://storage.googleapis.com/sparta-image.appspot.com/lecture/about.png"
+    
+    useEffect(()=>{
+        navigation.setOptions({
+            title:"소개 페이지",
+            headerStyle: {
+                backgroundColor: '#1F266A',
+                shadowColor: "#1F266A",
+            },
+            headerTintColor: "#fff",
+        })
+    },[])
+    return (
+        <View style={styles.container}>
+             <StatusBar style="light" />
+            <Text style={styles.title}>HI! 스파르타코딩 앱개발 반에 오신것을 환영합니다</Text>
+        
+            
+            <View style={styles.textContainer}>
+                <Image style={styles.aboutImage} source={{uri:aboutImage}} resizeMode={"cover"}/>
+                <Text style={styles.desc01}>많은 내용을 간결하게 담아내려 노력했습니다!</Text>
+                <Text style={styles.desc02}>꼭 완주 하셔서 꼭 여러분것으로 만들어가시길 바랍니다</Text>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>여러분의 인스타계정</Text>
+                </TouchableOpacity>
+            </View>
+        </View>)
 }
 
 const styles = StyleSheet.create({
-
-    container : {
-        backgroundColor : '#000080',
-        paddingLeft : 15,
-        paddingRight : 15
+    container: {
+        flex:1,
+        backgroundColor:"#1F266A",
+        alignItems:"center"
+      
     },
-
-    title : {
-        fontSize: 29,
-        fontWeight: '700',
-        marginTop : 100,
-        color : 'white',
-        textAlign : 'center'
+    title: {
+        fontSize:30,
+        fontWeight:"700",
+        color:"#fff",
+        paddingLeft:30,
+        paddingTop:20,
+        paddingRight:30
     },
-    imageView : {
-
-        backgroundColor:"#ffff",
-        borderColor:"deeppink",
-        borderRadius:15,
-        marginTop: 50,
-        height : 500
+    textContainer: {
+        width:300,
+        height:500,
+        backgroundColor:"#fff",
+        marginTop:50,
+        borderRadius:30,
+        justifyContent:"center",
+        alignItems:"center"
     },
-
-    mainImg : {
-        alignSelf:"center",
-        width:'50%',
+    aboutImage:{
+        width:150,
         height:150,
-        margin : 30,
-        borderRadius : 60,
+        borderRadius:30
     },
+    desc01: {
+        textAlign:"center",
+        fontSize:20,
+        fontWeight:"700",
+        paddingLeft:22,
+        paddingRight:22
 
-    subTitle1 : {
-        fontSize : 20, 
-        margin : 10,
-        fontWeight: '600',
-        textAlign : 'center'
-    }, 
-
-    subTitle2 : {
-        fontSize : 15, 
-        fontWeight: '500',
-        textAlign : 'center'
-    }, 
-    
-    button : {
-        alignSelf:"center",
-        backgroundColor:"#fdc453",
-        borderColor:"deeppink",
-        width:100,
-        height:50,
-        padding:15,
-        borderRadius:15,
-        margin:7    
-    }, 
-
-    buttonText : {
-        color : '#fff', 
-        fontWeight: '500',
     },
+    desc02: {
+        textAlign:"center",
+        fontSize:15,
+        fontWeight:"700",
+        padding:22
+    },
+    button:{
+        backgroundColor:"orange",
+        padding:20,
+        borderRadius:15
+    },
+    buttonText: {
+        color:"#fff",
+        fontSize:15,
+        fontWeight:"700"
+    }
 })
